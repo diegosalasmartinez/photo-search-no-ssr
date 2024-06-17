21e2410d599a585e88d9acd96fb6a4c3f8e1e91d
Diego <diesalasmart@gmail.com>
2024-06-17 13:12:00 -0500

    feat: redirect to home page when users click on header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link"
+
 export default function Header() {
   return (
     <header className="w-full bg-stone-900 flex items-center gap-2 leading-3 p-4">
-      <h1 className="text-2xl text-neutral-500 font-semibold uppercase">
+      <Link
+        href="/"
+        className="text-2xl text-neutral-500 font-semibold uppercase"
+      >
         Photo
         <span className="text-white">Search</span>
-      </h1>
+      </Link>
       <span className="text-neutral-500 text-xs">powered by Flickr</span>
     </header>
   )
